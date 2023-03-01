830a8dc33b35bd15d799f3962d4e320f9cf2c935
Victor <uberman59@gmail.com>
2023-03-01 23:37:22 +0500

    feat: complete basics

--- basic-types/index.js
+++ basic-types/index.js
@@ -24,3 +24,22 @@ function getFullNameByObj(userEntity) {
     return `${userEntity.firstName} ${userEntity.lastName}`;
 }
 console.log(getFullNameByObj(users));
+const skills = [1, 'string'];
+const id = skills[0];
+const skillName = skills[1];
+const arr = [1, 'sada', true, false];
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 1] = "SUCCESS";
+    StatusCode[StatusCode["IN_PROCESS"] = 2] = "IN_PROCESS";
+    StatusCode[StatusCode["FAILED"] = 3] = "FAILED";
+})(StatusCode || (StatusCode = {}));
+const res2 = {
+    message: 'Success',
+    statusCode: StatusCode.SUCCESS,
+};
+// 1- SUCCESS
+//2 - IN_PROCESS
+// 3- FAILED
+if (res2.statusCode === StatusCode.SUCCESS) {
+}
--- basic-types/index.ts
+++ basic-types/index.ts
@@ -30,3 +30,25 @@ function getFullNameByObj(userEntity: {
   return `${userEntity.firstName} ${userEntity.lastName}`;
 }
 console.log(getFullNameByObj(users));
+
+const skills: [number, string] = [1, 'string'];
+const id = skills[0];
+const skillName = skills[1];
+
+const arr: readonly [number, string, ...boolean[]] = [1, 'sada', true, false];
+enum StatusCode {
+  SUCCESS = 1,
+  IN_PROCESS,
+  FAILED,
+}
+const res2 = {
+  message: 'Success',
+  statusCode: StatusCode.SUCCESS,
+};
+
+// 1- SUCCESS
+//2 - IN_PROCESS
+// 3- FAILED
+
+if (res2.statusCode === StatusCode.SUCCESS) {
+}
